0a14bfc30b83990b13155b1f221f21f29e6895ab
Ernest Cui <ecuiships@126.com>
2024-04-28 21:58:12 +0800

    feat: tab page structure constructed

---
+++ MqttClient.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CLTLS_MQTT_GUI
+{
+    /// <summary>
+    /// Interaction logic for MqttClient.xaml
+    /// </summary>
+    public partial class MqttClient : UserControl
+    {
+        public MqttClient()
+        {
+            InitializeComponent();
+        }
+    }
+}
